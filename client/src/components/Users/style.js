ecaa4995c36a5bde4aabe84c2396ca0110d05989
shvetakumavat <shvetakumavat137.055@gmail.com>
2023-03-17 13:15:58 +0530

    new commit with build folder update

--- client/src/components/Users/style.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-export default makeStyles({
-    container: {
-        marginTop:"5px",
-  padding:"20px"
- }
-});
--- index.js
+++ index.js
@@ -7,16 +7,28 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-
-
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname,"./client/build")));
+
+app.get("*", function(req, res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
+
 
 const CONNECTION_URL = 'mongodb+srv://ShwetaKumavat:admin@123@sharecode.gam4jvf.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
